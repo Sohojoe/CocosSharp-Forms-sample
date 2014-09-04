8b272070d5ddab2cb4f806b5b5a0f967d40e94df
Joe Booth <joe@joebooth.com>
2014-09-03 18:07:27 -0700

    Create .git

---
+++ TestCocos/ApplicationDelegate.cs
@@ -0,0 +1,42 @@
+﻿using CocosDenshion;
+using CocosSharp;
+
+namespace TestCocos
+{
+	public class ApplicationDelegate: CCApplicationDelegate
+	{
+		public override void ApplicationDidFinishLaunching (CCApplication application, CCWindow mainWindow)
+		{
+			application.PreferMultiSampling = false;
+			application.ContentRootDirectory = "Content";
+
+			mainWindow.SupportedDisplayOrientations = CCDisplayOrientation.Portrait | CCDisplayOrientation.LandscapeLeft | CCDisplayOrientation.LandscapeRight;
+
+			application.ContentSearchPaths.Add ("hd");
+
+
+			CCSimpleAudioEngine.SharedEngine.PreloadEffect ("Sounds/tap");
+
+			CCScene scene = GameStartLayer.GameStartLayerScene (mainWindow);
+			mainWindow.RunWithScene (scene);
+		}
+
+		public override void ApplicationDidEnterBackground (CCApplication application)
+		{
+			// stop all of the animation actions that are running.
+			application.Paused = true;
+
+			// if you use SimpleAudioEngine, your music must be paused
+			CCSimpleAudioEngine.SharedEngine.PauseBackgroundMusic ();
+		}
+
+		public override void ApplicationWillEnterForeground (CCApplication application)
+		{
+			application.Paused = false;
+
+			// if you use SimpleAudioEngine, your background music track must resume here. 
+			CCSimpleAudioEngine.SharedEngine.ResumeBackgroundMusic ();
+		}
+	}
+}
+
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+using CocosSharp;
+using GoneBananas;
+
+
+namespace TestCocos.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+		/*
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+
+			if (true) {
+				// create a new window instance based on the screen size
+				window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+				// If you have defined a root view controller, set it here:
+				// window.RootViewController = myViewController;
+
+				// make the window visible
+				window.MakeKeyAndVisible ();
+				return true;
+
+			} else {
+				Forms.Init ();
+
+
+				window = new UIWindow (UIScreen.MainScreen.Bounds);
+			
+				window.RootViewController = App.GetMainPage ().CreateViewController ();
+				window.MakeKeyAndVisible ();
+				return true;
+			
+			}
+		}
+*/
+		public override void FinishedLaunching (UIApplication app)
+		{
+			var application = new CCApplication ();
+			application.ApplicationDelegate = new GoneBananasApplicationDelegate ();
+			application.StartGame ();
+		}
+	}
+}
+
